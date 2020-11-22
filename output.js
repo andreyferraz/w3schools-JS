e002dccb0fdb95b09b811d0ec36f42485735e344
andreyferraz <andrey.developer@hotmail.com>
2020-11-21 22:27:44 -0300

    entendendo saídas em js

---
+++ output.js
@@ -0,0 +1,18 @@
+function exibirAlerta(){
+    window.alert (1570 / 2.5);
+}
+//escrever a palavra chave window é opcional
+//eu posso escrever apenas alert, e o browser 
+//vai reconhecer
+
+// para fazermos depuração temos o console.log 
+function calculoExe(){
+    console.log(20 / 2);
+}
+//para depurar basta ir no console do browser
+//passar a função e dar enter, dai ela será executada
+
+//função para imprimir o contéudo da página
+function  imprimindoConteudo(){
+    window.print()
+}
